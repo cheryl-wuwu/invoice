4d27ca9d15bc58cc5ab7e5eda0fd76901b37f9a8
cheryl-wuwu <cheryl.wu@autech.com.tw>
2026-05-26 15:53:50 +0800

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -34,9 +34,9 @@ export async function getSession() {
 
 export async function getUserRole(email) {
   const { data, error } = await supabase
-    //.from('user_roles')
-    //.select('role')
-    //.eq('email', email)
+    .from('user_roles')
+    .select('role')
+    .eq('email', email)
     .single()
   if (error || !data) return 'staff'   // 預設一般人員
   return data.role
